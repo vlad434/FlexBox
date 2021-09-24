f004e711a88a4fe1af4ef06265a6b8072088b0b6
vlad434 <32937795+vlad434@users.noreply.github.com>
2021-09-24 19:43:41 +0300

    Add files via upload

---
+++ flex.js
@@ -0,0 +1,106 @@
+// Container 1 - Justify Content
+
+document.getElementById("start").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--justify-content-first",
+    "flex-start"
+  );
+});
+
+document.getElementById("center").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--justify-content-first",
+    "center"
+  );
+});
+
+document.getElementById("end").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--justify-content-first",
+    "flex-end"
+  );
+});
+
+document.getElementById("around").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--justify-content-first",
+    "space-around"
+  );
+});
+
+document.getElementById("between").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--justify-content-first",
+    "space-between"
+  );
+});
+
+
+
+// Container 2 - Justify Content
+
+document.getElementById("startSecond").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--justify-content-second",
+    "flex-start"
+  );
+});
+
+document.getElementById("centerSecond").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--justify-content-second",
+    "center"
+  );
+});
+
+document.getElementById("endSecond").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--justify-content-second",
+    "flex-end"
+  );
+});
+
+document.getElementById("aroundSecond").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--justify-content-second",
+    "space-around"
+  );
+});
+
+document.getElementById("betweenSecond").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--justify-content-second",
+    "space-between"
+  );
+});
+
+
+
+
+// Container 2 - Align Items
+
+document.getElementById("alignStart").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--align-items-second",
+    "flex-start"
+  );
+});
+
+document.getElementById("alignCenter").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--align-items-second",
+    "center"
+  );
+});
+
+document.getElementById("alignEnd").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--align-items-second",
+    "flex-end"
+  );
+});
+
+
+
+
+
